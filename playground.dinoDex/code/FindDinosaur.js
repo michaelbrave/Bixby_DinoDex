ef70464cb37c449c3fc0038c94da5250092374dd
Michael Brave <michaelbrave@outlook.com>
2019-12-18 13:30:59 -0800

    case fix

--- playground.dinoDex/code/FindDinosaur.js
+++ playground.dinoDex/code/FindDinosaur.js
@@ -1,31 +1,31 @@
 exports.tests = []
 exports.preconditions = []
 const allData = require("./lib/dinosaur");
-exports.function = function(nAME, fACT, tRIVIA, period, species, height, weight, location) {
+exports.function = function(dinoName, fact, trivia, period, species, height, weight, location) {
   var records = allData
-  if (nAME) {
-    records = records.filter(record => record.nAME.toLowerCase().indexOf(nAME.toLowerCase()) >= 0)
+  if (dinoName) {
+    records = records.filter(record => record.dinoName.toLowerCase().indexOf(dinoName.toLowerCase()) >= 0)
   }
-  if (fACT) {
-    records = records.filter(record => record.fACT.toLowerCase().indexOf(fACT.toLowerCase()) >= 0)
+  if (fact) {
+    records = records.filter(record => record.fact.toLowerCase().indexOf(fact.toLowerCase()) >= 0)
   }
-  if (tRIVIA) {
-    records = records.filter(record => record.tRIVIA.toLowerCase().indexOf(tRIVIA.toLowerCase()) >= 0)
+  if (trivia) {
+    records = records.filter(record => record.trivia.toLowerCase().indexOf(trivia.toLowerCase()) >= 0)
   }
   if (period) {
-    records = records.filter(record => record.period.toLowerCase().indexOf(tRIVIA.toLowerCase()) >= 0)
+    records = records.filter(record => record.period.toLowerCase().indexOf(trivia.toLowerCase()) >= 0)
   }
   if (species) {
-    records = records.filter(record => record.species.toLowerCase().indexOf(tRIVIA.toLowerCase()) >= 0)
+    records = records.filter(record => record.species.toLowerCase().indexOf(trivia.toLowerCase()) >= 0)
   }
   if (height) {
-    records = records.filter(record => record.height.toLowerCase().indexOf(tRIVIA.toLowerCase()) >= 0)
+    records = records.filter(record => record.height.toLowerCase().indexOf(trivia.toLowerCase()) >= 0)
   }
   if (weight) {
-    records = records.filter(record => record.weight.toLowerCase().indexOf(tRIVIA.toLowerCase()) >= 0)
+    records = records.filter(record => record.weight.toLowerCase().indexOf(trivia.toLowerCase()) >= 0)
   }
   if (location) {
-    records = records.filter(record => record.location.toLowerCase().indexOf(tRIVIA.toLowerCase()) >= 0)
+    records = records.filter(record => record.location.toLowerCase().indexOf(trivia.toLowerCase()) >= 0)
   }
   return records
 }
